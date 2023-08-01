969ea472297ec78841b90d0186f64c43740faeae
VirLg <108211942+VirLg@users.noreply.github.com>
2023-08-01 18:34:12 +0300

    add form

--- src/components/pages/moviesPage/MoviesPage.jsx
+++ src/components/pages/moviesPage/MoviesPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const MoviesPage = () => {
-  return <div>MoviesPage</div>;
+  return (
+    <form>
+      <label>
+        Search movie
+        <input
+          type="text"
+          // value="value"
+          name="search"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        />
+        <button type="button"> Search</button>
+      </label>
+    </form>
+  );
 };
 
 export default MoviesPage;
